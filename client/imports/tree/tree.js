ce9f2f051fc2d4b52a4bc06b0184c2a6b34f7925
Byron Batteson <byronbatteson@gmail.com>
2016-07-03 12:58:38 +0200

    Added delete functionality to the tree view; making it easier to track your work visually.

--- client/imports/tree/tree.js
+++ client/imports/tree/tree.js
@@ -10,12 +10,24 @@ import './tree.html';
 let svg;
 
 /**
+ * When the treeContainer template is created, initialise the selected node reactive variable.
+ */
+Template.treeContainer.onCreated(function() {
+    this.selectedNode = new ReactiveVar(null);
+});
+
+/**
  * When the treeContainer template is rendered, get the data from the server and display it
  */
 Template.treeContainer.onRendered(function(){
+    var instance = this;
     svg = new trees.SVG('treesSVG', {
         width: '100%',
         height: '92%'
+    });
+
+    svg.setSelectedAction(function(node) {
+        instance.selectedNode.set(node);
     });
 
     Meteor.call('desktopTreeData', function(e, elements) {
@@ -36,26 +48,41 @@ Template.treeContainer.onRendered(function(){
     });
 });
 
+Template.on
+
 /**
  * Sets the anchor of the nodes, there are 3 selection modes.
  */
 Template.treeContainer.events({
-    "click #none": function(e) {
+    'click #none': function(e) {
         svg.setAnchor('none');
         $('#mainFAB').text('play_arrow');
         $('#mainFABColor').removeClass('red').removeClass('green').addClass('blue');
         $('.fixed-action-btn').closeFAB();
     },
-    "click #children": function(e) {
+    'click #children': function(e) {
         svg.setAnchor('children');
         $('#mainFAB').text('fast_forward');
         $('#mainFABColor').removeClass('red').removeClass('blue').addClass('green');
         $('.fixed-action-btn').closeFAB();
     },
-    "click #descendents": function(e) {
+    'click #descendents': function(e) {
         svg.setAnchor('descendents');
         $('#mainFAB').text('skip_next');
         $('#mainFABColor').removeClass('blue').removeClass('green').addClass('red');
         $('.fixed-action-btn').closeFAB();
+    },
+    'click #delete': function(e, instance) {
+        var removedNode = svg.removeNode(instance.selectedNode.get(), false);
+        if(removedNode) {
+            Meteor.call('removeNode', removedNode.id);
+            instance.selectedNode.set(null);
+        }
+    }
+});
+
+Template.treeContainer.helpers({
+    selectedNode() {
+        return Template.instance().selectedNode.get();
     }
 });
